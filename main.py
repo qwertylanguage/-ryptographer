05645b4f20569b331aebd645d815173389e4bafd
denistkachev <deniskyn228@gmail.com>
2023-01-08 23:06:37 +0500

    Let's do that

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print('hi word')
+import pyAesCrypt
+
+password = input ('Enter your password ')
+
+pyAesCrypt.encryptFile('data.txt', 'data.txt.aes',password)
+
+
+'''дешифратор - decoder'''
+
+
+pyAesCrypt.decryptFile('data.txt.aes', 'data.txt',password)
